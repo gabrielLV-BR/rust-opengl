43460a337fe1671ef1c8a333d0bfab5ae99bbbd0
Gabriel L <gabriellovato15@gmail.com>
2023-03-25 14:15:40 -0300

    Removed servers

--- src/servers/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod mesh_server;
-pub mod program_server;
-
-pub trait AssetServer<T> {
-    type Handle;
-    fn store(&mut self, item: T) -> Self::Handle;
-    fn get(&self, key: Self::Handle) -> Option<&T>;
-}
